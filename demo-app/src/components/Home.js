43672beb796e19fe943fca81eb03b20bbcf82a95
Mkisecok <mahmutkisecok@gmail.com>
2021-12-30 15:10:12 +0100

    Add photos from api and Create Home.css file

--- demo-app/src/components/Home.js
+++ demo-app/src/components/Home.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import {useState,useEffect} from 'react'
+import { BallTriangle } from 'react-loading-icons'
+import axios from 'axios'
+import './Home.css'
 function Home() {
+const [photos, setPhotos] = useState([]);
+const [isLoading,setLoading]=useState(true)
+useEffect(()=>{
+    axios('https://jsonplaceholder.typicode.com/photos?albumId=1')
+    .then(res=>setPhotos(res.data))
+    .finally(()=> setLoading(false))
+    
+},[])
     return (
-        <div>
-            Home
+        
+        <div className='Home'>
+            <div className='icon'>{ isLoading && <BallTriangle />}</div>
+            
+                <div className='photos'>
+                {photos.map((photo)=>(
+                    
+                        <img key={photo.id} src={photo.url} alt={photo.title}/>
+                ))}
+                </div>
+            
         </div>
     )
 }
