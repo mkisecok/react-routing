d68ac5bd1de87668e664e258bf52eb22aebf657f
Mkisecok <mahmutkisecok@gmail.com>
2021-12-30 19:16:25 +0100

    Add Photo component for image detail through routing

---
+++ demo-app/src/components/Photo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useParams, Link }from 'react-router-dom'
+import {useEffect,useState} from 'react'
+import axios from 'axios'
+import './Photo.css'
+function Photo() {
+    const { id } = useParams();
+    const [photo, setPhoto] = useState({});
+    useEffect(()=>{
+        axios(`https://jsonplaceholder.typicode.com/photos/${id}`)
+        .then((res)=>setPhoto(res.data))
+    },[id])
+    return (
+        <div>
+           <h2>Photo Details</h2>
+           <div className='photo-detail'>{JSON.stringify(photo)}</div>
+        </div>
+    )
+}
+
+export default Photo
